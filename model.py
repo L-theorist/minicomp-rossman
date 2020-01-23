b82f9e0aec33e576ee9857a7a434830418c65667
gitMantas <mantas.mulokas@gmail.com>
2020-01-23 16:52:26 +0100

    Edited model.py

--- model.py
+++ model.py
@@ -2,7 +2,7 @@
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.datasets import make_regression
-def model(data, features, target, fraction=1): #takes (clean) data, list of features and target/list of targets if needed
+def model_regression(data, features, target, fraction=1): #takes (clean) data, list of features and target/list of targets if needed
     regr = RandomForestRegressor(max_depth=4, random_state=1, bootstrap=True)  #bootstramp True means samples with replacement
     regr.fit(data.loc[:, features].values, data[target].values.reshape(-1,1))
     RandomForestRegressor(max_depth=4, n_estimators=500, random_state=42)
