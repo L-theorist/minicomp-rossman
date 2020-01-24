8fb7e9e0934784f5d4976e8eb2e722590efe66d1
Filipp <git@filip.email>
2020-01-24 11:26:20 +0100

    modified model.py

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.datasets import make_regression
 from sklearn.linear_model import LinearRegression
 
-def model_RFR(X, y, m=4 fraction=1): #takes (clean) data, list of features and target/list of targets if needed
+def model_RFR(X, y, m=4, fraction=1): #takes (clean) data, list of features and target/list of targets if needed
     regr = RandomForestRegressor(max_depth=m, random_state=1, bootstrap=True)  #bootstramp True means samples with replacement
     regr.fit(X.values, y.values.reshape(-1,1))
     #RandomForestRegressor(max_depth=4, n_estimators=500, random_state=42)
