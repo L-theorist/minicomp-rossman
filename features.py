1177c99ffc3905eff4539a0f4dc11d2ed1cd1c55
gitMantas <mantas.mulokas@gmail.com>
2020-01-24 15:10:33 +0100

    Add features.py, main.py, RunModelOnTest.ipnb

--- features.py
+++ features.py
@@ -1,6 +1,8 @@
 import pandas as pd
 #import datetime dt
 
+features = ['DayOfWeek', 'WeekNumber', 'Month', 'DayOfYear', 'SalesPerCustomer', 'Customers']
+
 def df_add_features(df):
     df['WeekNumber'] = df['Date'].dt.week
     df['Month'] = df['Date'].dt.month
